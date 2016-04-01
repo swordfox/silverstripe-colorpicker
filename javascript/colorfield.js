6307820e4c603e507f4ac26ae73ecb736e8589c1
Graham <graham@swordfoxdesign.co.nz>
2016-04-01 13:20:17 +1300

    Update colorfield.js
    So it initializes through ajax requests.

--- javascript/colorfield.js
+++ javascript/colorfield.js
@@ -1,42 +1,49 @@
-var ColorFieldHandler = function(){
-	var $ = jQuery;
-	
-	this.initAll = function(){
-		var fnc = this.initById;
-		$('.ColorPickerInput').each(function(){
-			fnc('#' + $(this).attr('id'));
-		});
-	}
-	
-	this.initById = function(id){
-		$(id).ColorPicker({
-			onChange: function(hsb, hex, rgb) {
-				var mid = (rgb.r + rgb.g + rgb.b) / 3;
-				var col = mid > 127 ? '#000000' : '#ffffff';
-				$(id).val(hex).css({color:col, backgroundColor:'#' + hex});
-			},
-			onBeforeShow: function () {
+(function($) {
+	var ColorFieldHandler = function(){
+		var $ = jQuery;
+		
+		this.initAll = function(){
+			var fnc = this.initById;
+			$('.ColorPickerInput').each(function(){
+				fnc('#' + $(this).attr('id'));
+			});
+		}
+		
+		this.initById = function(id){
+			$(id).ColorPicker({
+				onChange: function(hsb, hex, rgb) {
+					var mid = (rgb.r + rgb.g + rgb.b) / 3;
+					var col = mid > 127 ? '#000000' : '#ffffff';
+					$(id).val(hex).css({color:col, backgroundColor:'#' + hex});
+				},
+				onBeforeShow: function () {
+					$(this).ColorPickerSetColor(this.value);
+				}
+			}).bind('keyup', function(){
 				$(this).ColorPickerSetColor(this.value);
-			}
-		}).bind('keyup', function(){
-			$(this).ColorPickerSetColor(this.value);
-		});
+			});
+		}
 	}
-}
+	
+	CFHandler = new ColorFieldHandler();
+	
+	//check if prototype wrapper is required.
+	if(typeof Behaviour == 'object'){
+		Behaviour.register({
+			'.ColorPickerInput' : {
+				initialise : function(){
+					CFHandler.initById('#' + this.id);
+				}
+			}	
+		});
+		
+	}
 
-CFHandler = new ColorFieldHandler();
-
-//check if prototype wrapper is required.
-if(typeof Behaviour == 'object'){
-	Behaviour.register({
-		'.ColorPickerInput' : {
-			initialise : function(){
-				CFHandler.initById('#' + this.id);
+	$.entwine('ss', function($) {	
+		$(".ColorPickerInput").entwine({
+			onmatch: function() {
+				CFHandler.initAll();
 			}
-		}	
+		});
 	});
-}
-
-jQuery(document).ready(function(){
-	CFHandler.initAll();
-});
+})(jQuery);
